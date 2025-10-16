e964d857492ffdbe8837619513acb47b879d4ef3
BilionBoy <dhiogoandre26@gmail.com>
2025-10-16 08:26:48 -0400

    arquivos traducoa distritos

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/index'
+  
+  # Rotas Scaffold
   resources :distritos
   resources :bairros
   resources :municipios
   resources :estados
   resources :paises
   resources :unidades_medida
-  root 'home#index'
-  get 'home/index'
-
-  # Rotas Scaffold
 
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
