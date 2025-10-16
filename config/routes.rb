5d3d8daab6da9c3d883d5f132df2d3af086d3ad4
BilionBoy <dhiogoandre26@gmail.com>
2025-10-16 08:46:45 -0400

    wip: preparando sidebar admnistrativo pra empresas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :empresas
   root 'home#index'
   get 'home/index'
   
@@ -11,6 +10,7 @@ Rails.application.routes.draw do
   resources :paises
   resources :unidades_medida
   resources :status_empresas
+  resources :empresas
 
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
