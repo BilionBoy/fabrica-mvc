c6d19f7944dd3d8e8e59c569454e38be484a568c
BilionBoy <dhiogoandre26@gmail.com>
2025-10-16 08:20:09 -0400

    inflections: bairros e arquivos de traducao

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -4,6 +4,7 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.irregular 'pais', 'paises'
   inflect.irregular 'estado', 'estados'
   inflect.irregular 'municipio', 'municipios'
+  inflect.irregular 'bairro',    'bairros'
   inflect.irregular 'categoria', 'categorias'
   inflect.irregular 'produto', 'produtos'
   inflect.irregular 'unidade_medida', 'unidades_medida'
