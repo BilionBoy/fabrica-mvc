2fd093481acd1559012cf11fe72b8368147cd2b8
BilionBoy <dhiogoandre26@gmail.com>
2025-10-16 08:14:07 -0400

    feat: inflections de municipios pra traducao doa ctive record

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -3,6 +3,7 @@
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.irregular 'pais', 'paises'
   inflect.irregular 'estado', 'estados'
+  inflect.irregular 'municipio', 'municipios'
   inflect.irregular 'categoria', 'categorias'
   inflect.irregular 'produto', 'produtos'
   inflect.irregular 'unidade_medida', 'unidades_medida'
